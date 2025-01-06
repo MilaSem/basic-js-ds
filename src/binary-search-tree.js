a11fb6b8a8975d4eb064a5ad04f116c69a9093ae
MilaSem <milafoxsem@gmail.com>
2025-01-06 23:40:20 +0300

    feat: implement root, min, max methods for BST

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,16 +1,18 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require("../extensions/list-tree.js");
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
+  constructor() {
+    this.tree = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.tree;
   }
 
   add(/* data */) {
@@ -32,18 +34,35 @@ class BinarySearchTree {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
+  _min(node) {
+    while (node.left) {
+      node = node.left;
+    }
+    return node.data;
+  }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.tree) {
+      return this._min(this.tree);
+    }
+    return null;
+  }
+
+  _max(node) {
+    while (node.right) {
+      node = node.right;
+    }
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.tree) {
+      return this._max(this.tree);
+    }
+    return null;
   }
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
